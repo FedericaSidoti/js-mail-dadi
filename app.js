f271f859dd6af7edbdd7c1a6202314e915dd531d
FedericaSidoti <federicasidoti21@gmail.com>
2023-10-09 16:18:47 +0200

    ADD- check on emails and stamp message

---
+++ app.js
@@ -0,0 +1,34 @@
+// - crea un array di mail
+const arrayEmails = [
+'frodobaggins@gmail.com',
+'bilbobaggins@gmail.com',
+'pipinotuc@gmail.com',
+'merrybrandibuck@gmail.com',
+'legolasverdefoglia@gmail.com',
+'aragornelessar@gmail.com'
+]
+
+
+
+const usersNumber = arrayEmails.length; 
+
+// - chiedi all'utente la mail
+let askMail = prompt('Scrivi qui la tua mail');
+
+// - PER OGNI mail nell'array, fai un confronto con la mail dell'utente
+
+let currentMail
+let message = 'accesso negato'
+
+for (let i = 0 ; i < usersNumber; i ++) {
+    const currentNumber = i; 
+    currentMail = arrayEmails[i]; 
+
+    if (currentMail == askMail) {
+        message = 'accesso riuscito'
+}
+
+}
+
+console.log(message)
+
